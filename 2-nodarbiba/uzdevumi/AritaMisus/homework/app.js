a7b67493bf64c3ea722f712886870a53cf8b770a
CharisDev <99430685+CharisDev@users.noreply.github.com>
2023-04-09 18:01:19 +0300

    created action elements #2 and #3

--- 2-nodarbiba/uzdevumi/AritaMisus/homework/app.js
+++ 2-nodarbiba/uzdevumi/AritaMisus/homework/app.js
@@ -1,8 +1,25 @@
 const actionElement = document.getElementById("action-el");
+const actionElement2 = document.getElementById("action-el-2");
+const actionElement3 = document.getElementById("action-el-3");
+
 
 const changeStyle = () => {
 actionElement.style.backgroundColor = "black";
 actionElement.style.width = "200px";
 actionElement.style.height = "200px";
+
+}
+
+const changeStyle2 = () => {
+actionElement2.style.backgroundColor = "blue";
+actionElement2.style.width = "250px";
+actionElement2.style.height = "250px";
+
+}
+
+const changeStyle3 = () => {
+actionElement3.style.backgroundColor = "red";
+actionElement3.style.width = "80px";
+actionElement3.style.height = "80px";
 
 }
