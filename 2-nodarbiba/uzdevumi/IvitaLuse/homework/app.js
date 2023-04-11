4396874320c2887fce410daebc12512c434d6953
IvitaLuse <ivita.luse@gmail.com>
2023-04-11 22:04:05 +0300

    2.nodarbiba, homework

--- 2-nodarbiba/uzdevumi/IvitaLuse/homework/app.js
+++ 2-nodarbiba/uzdevumi/IvitaLuse/homework/app.js
@@ -4,5 +4,21 @@ const changeStyle = () => {
 actionElement.style.backgroundColor = "black";
 actionElement.style.width = "200px";
 actionElement.style.height = "200px";
+}
 
+
+const action2 = document.getElementById("action2");
+
+const changeStyle2 = () => {
+action2.style.animationName = "example";
+action2.style.animationDuration = "4s";
+action2.style.animationDely = "4s";
+}
+
+const action3 = document.getElementById("action3");
+
+const changeStyle3 = () => {
+action3.style.margin = "195px";
+action3.style.transition = "3s"
+action3.style.backgroundColor = "pink";
 }
