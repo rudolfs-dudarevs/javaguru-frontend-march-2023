78c9ba601cd041486b55f023c11b4117ee8b4705
karlis-ulmanis <ulmens1@gmail.comm>
2023-04-13 18:07:39 +0300

    2. nodarbības mājasdarbs

--- 2-nodarbiba/uzdevumi/KarlisUlmanis/homework/app.js
+++
@@ -1,8 +0,0 @@
-const actionElement = document.getElementById("action-el");
-
-const changeStyle = () => {
-actionElement.style.backgroundColor = "black";
-actionElement.style.width = "200px";
-actionElement.style.height = "200px";
-
-}
