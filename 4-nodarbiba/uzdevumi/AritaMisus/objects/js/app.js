bcd92c4aea3383d7d60dc5d7815509e229d41f22
CharisDev <99430685+CharisDev@users.noreply.github.com>
2023-04-24 13:16:29 +0300

    created object

--- 4-nodarbiba/uzdevumi/AritaMisus/objects/js/app.js
+++ 4-nodarbiba/uzdevumi/AritaMisus/objects/js/app.js
@@ -1,5 +1,27 @@
 // Izveidot Objektu, kas saturēs informāciju par jūsu personīgo vai izdomāta tēla CV.
 
+const cvNameSurname = {
+  firstName: "James",
+  lastName: "McAvoy",
+  email: "james@mcavoy.com",
+  telNr: 1234567,
+  workExperience: ["Company1", "Company2", "Company3", "Company4"],
+  lanuguages: {
+    scot: {
+      name: "Scottish",
+      level: "Native",
+    },
+    eng: {
+      name: "English",
+      level: "Native",
+    },
+    sse: {
+      name: "Standard Scottish English",
+      level: "Native",
+    },
+  },
+};
+
 // 1. Mainiga nosaukums, kurš satur Objektu ir vardsUzvards (jūsu vārds/uzvārds)
 // 2. CV objekts satur 3 īpašības (properties) ar string tipa vērtību:
 //    -- firstName
@@ -14,7 +36,7 @@
 //    -- languages
 //    Languages objektā ir 3 īpašības ar nosaukumiem lv, rus, eng, piem.
 //    Katra no languages objekta īpašībām ir objekts, kas satur 2 īpašības - name, level.
-//    
+//
 //    languages: {
 //          lv: {
 //             name: "Latviešu",
@@ -22,15 +44,20 @@
 //          },
 //          rus: {
 //              ...
-//          }, 
+//          },
 //          eng: {
 //              ...
 //          }
 //    }
 //
 // 6. Izvadīt konsolē CV objekta īpašību firstName un lastName apvienojumu.
+console.log(`${cvNameSurname.firstName} ${cvNameSurname.lastName}`);
 // 7. Izvadīt konsolē CV objekta īpašības workExperience 2 elementu (index 1).
+console.log(cvNameSurname.workExperience[1]);
 // 8. Izvadīt konsolē CV objekta īpašības languages objekta eng īpašības name un level apvienojumu.
+console.log(
+  `${cvNameSurname.lanuguages.eng.name} - ${cvNameSurname.lanuguages.eng.level}`
+);
 //    -- piem., vardsUzvards.languages.lv objekta īpašību apvienojums izskatītos šādi:
-//       Latviešu - Native 
+//       Latviešu - Native
 //       Un tiktu panākta ar string literal pierakstu `${vardsUzvards.languages.lv.name} - ${vardsUzvards.languages.lv.level}`
