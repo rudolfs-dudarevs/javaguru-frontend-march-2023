2c2874b8f8b5c7549fe3c3a39c28bdca4c33dd96
Macbook Pro <macbookpro@Dins-MacBook-Pro.local>
2023-05-09 23:03:58 +0300

    5.majasdarbs

--- 5-nodarbiba/uzdevumi/DinsKalnins/js/app.js
+++ 5-nodarbiba/uzdevumi/DinsKalnins/js/app.js
@@ -12,80 +12,84 @@
 const itemInput = document.getElementById("item-input");
 const addBtn = document.getElementById("add-btn");
 const shoppingListContainer = document.getElementById("shopping-list");
+const removeBtn = document.getElementById("remove-btn");
 // Dotais mainīgais priekš iepirkumu saraksta:
 let shoppingList;
 
 // ----------------------------------------------------------------------------
-// 1. Iegūstam no localStorage ierakstu(value) ar nosaukumu(key) "shoppingList" 
+// 1. Iegūstam no localStorage ierakstu(value) ar nosaukumu(key) "shoppingList"
 
-// shoppingListAsString = 
-
+let getList = localStorage.getItem("shoppingList", shoppingList);
+console.log(getList);
 // --------------------------------------------------------
 // 2. ar JSON.parse() metodi pārvēršam shoopingListAsString
 //    no JSON string atpakaļ uz masīvu(array).
 
-// shoppingList = 
+let shoppingListAsString = localStorage.getItem("shoppingList");
 
 // Ja shooppingList nav atrasts local storage,
 // tas tiek iestatīts kā tukšs masīvs, kurā saglabāt shopping list ierakstus.
-if(!shoppingList) {
-    // Ja shoppingList tiks atrasts localStorage, šis kods neizpildīsies,
-    // jo mums jau ir masīvs ar datiem, kas iegūts no localStorage.
-    shoppingList = [];
+if (shoppingListAsString) {
+  shoppingList = JSON.parse(shoppingListAsString);
+} else {
+  shoppingList = [];
 }
+// Ja shoppingList tiks atrasts localStorage, šis kods neizpildīsies,
+// jo mums jau ir masīvs ar datiem, kas iegūts no localStorage.
 
 // ---------------------------------------
 // 3. Izveidojam funkcijas loģiku, kas tiks izpildīta uz katra addBtn klikšķa.
 const addToShoppingList = () => {
-    // Iegūstam vērtību no input HTML elementa jeb itemInput mainīgā.
-    // 3.1
-    // let item = 
+  // Iegūstam vērtību no input HTML elementa jeb itemInput mainīgā.
+  // 3.1
+  let item = itemInput.value;
 
-    // Iestatīt itemInput.value kā tukšu string (notīrīt esošo tekstu no ievdlauka).
-    // 3.2
-    // itemInput.value = 
+  // Iestatīt itemInput.value kā tukšu string (notīrīt esošo tekstu no ievdlauka).
+  // 3.2
+  itemInput.value = "";
 
-    // shoppingList masīvā ievietojam item mainīgo. Izmantot masīva push() metodi.
-    // 3.3
-    // shoppingList
+  // shoppingList masīvā ievietojam item mainīgo. Izmantot masīva push() metodi.
+  // 3.3
+  shoppingList.push(item);
 
-    // Uz katra pogas klikšķa jāizsauc funkciju saveStorage() un renderShoppingList()
-    // Šī funkcija saglabās iekš local storage shoppingList masīvu ka JSON
-    saveStorage();
-    // Šī funkcija izveidos un ievietos HTML priekš katra shoppingList masīva elementa.
-    renderShoppingList();
-}
+  // Uz katra pogas klikšķa jāizsauc funkciju saveStorage() un renderShoppingList()
+  // Šī funkcija saglabās iekš local storage shoppingList masīvu ka JSON
+  saveStorage();
+  // Šī funkcija izveidos un ievietos HTML priekš katra shoppingList masīva elementa.
+  renderShoppingList();
+};
 
 // --------------------------------------------------------------------------------------
 // 4. Izveidot funkcijas loģiku, lai saglabātu localStorage shoppingList masīvu kā JSON.
 const saveStorage = () => {
-    // Izmantojam JSON.stringify(), lai pārvērstu shoppingList masīvu JSON string formātā.
-    // 4.1
-    // let shoppingListAsJSON = 
-    
-    // Iekš localStorage saglabājam shoppingListAsJSON mainīgo ar nosaukumu "shoppingList"
-    // 4.2
-
-}
+  // Izmantojam JSON.stringify(), lai pārvērstu shoppingList masīvu JSON string formātā.
+  // 4.1
+  let shoppingListAsJSON = JSON.stringify(shoppingList);
+  // Iekš localStorage saglabājam shoppingListAsJSON mainīgo ar nosaukumu "shoppingList"
+  // 4.2
+  localStorage.setItem("shoppingList", shoppingListAsJSON);
+};
 
 // ------------------------------------------------------------------
 // 5. Izveidot funkcijas loģiku, lai iterētu caur shoppingList masīvu
 //    un katram masīva elementam izveidotu HTML.
 const renderShoppingList = () => {
-    // String mainīgais, kurā ievietosim HTML priekš katra shoppingList masīva elementa.
-    let listToRender = '';
+  // String mainīgais, kurā ievietosim HTML priekš katra shoppingList masīva elementa.
+  let listToRender = "";
 
-    // 5.1 Iterēt cauri shopping list masīvam ar forEach ciklu.
-    //     Katram masīva elementam izpildīt sekojošo darbību:
-    //
-    //     listToRender += `
-    //          <li class="list-group-item">
-    //              ${item}
-    //          </li>
-    //      `;
+  // 5.1 Iterēt cauri shopping list masīvam ar forEach ciklu.
+  //     Katram masīva elementam izpildīt sekojošo darbību:
 
-    shoppingListContainer.innerHTML = listToRender;
-}
+  shoppingList.forEach((item) => {
+    listToRender += `
+    <li class="list-group-item">
+    ${item}
+    </li>
+    `;
+  });
+
+  shoppingListContainer.innerHTML = listToRender;
+};
 
 // Lai shopping list no localStorage tikktu attēlots līdz ar lapas ielādi,
 // izsaucam renderShoppingList() metodi.
@@ -94,7 +98,14 @@ renderShoppingList();
 // -----------------------------------------------------------------------------
 // 6. mainīgajam addBtn uz klikšķa notikua piesaistīt funkciju addToShoppingList
 //    izmantot .addEventListener() metodi.
-addBtn.addEventListener('click', addToShoppingList);
+const clearShoppingList = () => {
+  localStorage.clear();
+  shoppingList = [];
+  renderShoppingList();
+};
+
+addBtn.addEventListener("click", addToShoppingList);
+removeBtn.addEventListener("click", clearShoppingList);
 
 // -----------------------------------------------------------------------------
 // 7. NAV OBLIGĀTI
@@ -105,6 +116,6 @@ addBtn.addEventListener('click', addToShoppingList);
 //      kas pārbauda ievadlauka vērtību:
 //      IF ievadlaukā ir teksts jeb tas nav tukšs string - izpildīt funkcijas saveStorage() un renderShoppingList()
 //      ELSE ievadlaukā ir tukšs string jeb "" - izmantot alert() un paziņot, kas ievadlaukā jāievada vērtība.
-//      
+//
 //      w3schools resurss par alert():
 //      https://www.w3schools.com/jsref/met_win_alert.asp
