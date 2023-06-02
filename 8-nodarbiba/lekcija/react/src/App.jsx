9c27402faa327c6cb9fe256748e234109ffc92f5
Rudolfs Dudarevs <rudolf.dudarevs@gmail.com>
2023-06-02 21:50:20 +0300

    homework

---
+++ 8-nodarbiba/lekcija/react/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from "./components/Footer/Footer"
+import './App.css';
+
+const App = () =>{
+    return (
+        <>
+            {/* Header goes here */}
+            <main>
+                {/* Some content */}
+            </main>
+            <Footer />
+        </>
+    )
+}
+
+export default App
