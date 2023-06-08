60ec75082f59f54bfb3f89a2a87ca1023aa0f8f9
Sandis Andrucis <sandis.andrucis@gmail.com>
2023-06-08 05:59:57 +0300

    react homework

---
+++ 8-nodarbiba/uzdevumi/SandisAndrucis/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import './App.css';
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+    )
+}
+
+export default App
---
+++ 8-nodarbiba/uzdevumi/SandisAndrucis/src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Header.css';
+
+const Header = () =>{
+    return (
+    <header id="header">
+        <h1 className="header__title">
+            Sandis Andrucis
+            <div className="header__subtitle">
+                Frontend developer
+            </div>
+        </h1>
+    </header>
+    )
+}
+
+export default Header
