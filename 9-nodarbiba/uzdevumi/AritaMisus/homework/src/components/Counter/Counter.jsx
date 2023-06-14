3dafcd6bc661fe09038a51770e64f73d24e82597
CharisDev <99430685+CharisDev@users.noreply.github.com>
2023-06-14 11:57:36 +0300

    useState and useEffect for card item display, card counter 1-10 + styling for grid controls, counter and cards

--- 9-nodarbiba/uzdevumi/AritaMisus/homework/src/components/Counter/Counter.jsx
+++ 9-nodarbiba/uzdevumi/AritaMisus/homework/src/components/Counter/Counter.jsx
@@ -1,19 +1,36 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Counter.css";
 
-// Saņemt no props funkciju handleCountChange, ko izmantot uz pogas klikšķa
-// Saņemt no props mainīgo count, kas attēlot pašreiozējo Counter vērtību
-const Counter = () => {   
-    return (
-        <div className="container">
-            {/* onClick piesaistīt handleCountChange - kā parametru funkcija padot 'increment' */}
-            <button className="button" onClick={() => {}}>+</button>
-            {/* ievietot count mainīgo */}
-            <p className="count">{5}</p>
-            {/* onClick piesaistīt handleCountChange - kā parametru funkcija padot 'decrament' */}
-            <button className="button" onClick={() => {}}>-</button>
-        </div>
-    )
-}
+const Counter = ({ handleCountChange, count }) => {
+  const decrement = () => {
+    if (count > 1) {
+      handleCountChange("decrement");
+    }
+  };
 
-export default Counter
+  const increment = () => {
+    if (count < 10) {
+      handleCountChange("increment");
+    }
+  };
+
+  return (
+    <div className="container">
+      <button
+        className="button minus"
+        onClick={() => handleCountChange("decrement")}
+      >
+        -
+      </button>
+      <p className="count">{count}</p>
+      <button
+        className="button plus"
+        onClick={() => handleCountChange("increment")}
+      >
+        +
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
--- 9-nodarbiba/uzdevumi/AritaMisus/homework/src/components/PostCard/PostCard.jsx
+++ 9-nodarbiba/uzdevumi/AritaMisus/homework/src/components/PostCard/PostCard.jsx
@@ -1,15 +1,15 @@
-import React from "react"
-import "./PostCard.css"
+import React from "react";
+import "./PostCard.css";
 
 // # Šajā rindiņā var redzēt, kā komponente saņem props "title" un "text"
-const PostCard = ({title, text}) => {
-    return (
-        <div className="post-card">
-            {/* Šādi izmantojam props "title" un "text" iekš komponentes */}
-            <p className="post-card__title">{title}</p>
-            <p className="post-card__text">{text}</p>
-        </div>
-    )
-}
+const PostCard = ({ title, text }) => {
+  return (gitgit 
+    <div className="post-card">
+      {/* Šādi izmantojam props "title" un "text" iekš komponentes */}
+      <p className="post-card__title">{title}</p>
+      <p className="post-card__text">{text}</p>
+    </div>
+  );
+};
 
-export default PostCard
+export default PostCard;
