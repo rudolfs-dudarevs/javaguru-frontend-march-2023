79db249afb774348b19c936663eca36bc7476425
CharisDev <99430685+CharisDev@users.noreply.github.com>
2023-06-14 12:23:33 +0300

    corrected typo

--- 9-nodarbiba/uzdevumi/AritaMisus/homework/src/components/PostCard/PostCard.jsx
+++ 9-nodarbiba/uzdevumi/AritaMisus/homework/src/components/PostCard/PostCard.jsx
@@ -3,7 +3,7 @@ import "./PostCard.css";
 
 // # Šajā rindiņā var redzēt, kā komponente saņem props "title" un "text"
 const PostCard = ({ title, text }) => {
-  return (gitgit 
+  return (
     <div className="post-card">
       {/* Šādi izmantojam props "title" un "text" iekš komponentes */}
       <p className="post-card__title">{title}</p>
