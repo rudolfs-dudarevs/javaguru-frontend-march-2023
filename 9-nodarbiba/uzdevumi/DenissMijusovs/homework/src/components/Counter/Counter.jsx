f1301cdc4ee797ca49e98ca799c46fd6288dbcd4
Deniss1988 <105513759+Deniss1988@users.noreply.github.com>
2023-06-18 18:59:05 +0300

    9-deniss-mijusovs

--- 9-nodarbiba/uzdevumi/DenissMijusovs/homework/src/components/Counter/Counter.jsx
+++ 9-nodarbiba/uzdevumi/DenissMijusovs/homework/src/components/Counter/Counter.jsx
@@ -3,15 +3,15 @@ import "./Counter.css";
 
 // Saņemt no props funkciju handleCountChange, ko izmantot uz pogas klikšķa
 // Saņemt no props mainīgo count, kas attēlot pašreiozējo Counter vērtību
-const Counter = () => {   
+const Counter = ({handleCountChange,count}) => {   
     return (
         <div className="container">
             {/* onClick piesaistīt handleCountChange - kā parametru funkcija padot 'increment' */}
-            <button className="button" onClick={() => {}}>+</button>
+            <button className="button" onClick={() => handleCountChange("increment")}>+</button>
             {/* ievietot count mainīgo */}
-            <p className="count">{5}</p>
+            <p className="count">{count}</p>
             {/* onClick piesaistīt handleCountChange - kā parametru funkcija padot 'decrament' */}
-            <button className="button" onClick={() => {}}>-</button>
+            <button className="button" onClick={() => handleCountChange("decrement")}>-</button>
         </div>
     )
 }
