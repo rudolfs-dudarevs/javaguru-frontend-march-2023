cb13b3fe1e7d775b7aacb6251620b633d4251484
Sandis Andrucis <sandis.andrucis@gmail.com>
2023-07-01 05:38:02 +0300

    hide nav on scroll and contactUs update

--- 10-nodarbiba/uzdevumi/SandisAndrucis/sandis-andrucis-galadarbs/src/components/Header/Header.jsx
+++ 10-nodarbiba/uzdevumi/SandisAndrucis/sandis-andrucis-galadarbs/src/components/Header/Header.jsx
@@ -4,10 +4,33 @@ import logo from "../../assets/BellaLogo.png";
 import { NavLink } from "react-router-dom";
 import SlideMenu from "../SlideMenu/SlideMenu";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from 'react';
 
 const Header = () => {
+
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true)
+  
+  const handleScroll = () => {
+      const currentScrollPos = window.scrollY
+  
+      if(currentScrollPos > prevScrollPos){
+          setVisible(false)
+      }else{
+          setVisible(true)
+      }
+  
+      setPrevScrollPos(currentScrollPos)
+  }
+  
+  useEffect( () => {
+      window.addEventListener('scroll', handleScroll);
+  
+      return () => window.removeEventListener('scroll', handleScroll)
+  })  
+
   return (
-    <header className="header">
+    <header className={`header ${visible ? "header__visible" : "header__hidden"}`}>
       <Link to="/">
         <img src={logo} className="header__logo"></img>
       </Link>
