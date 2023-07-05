298d88f5ffc8d5d37cf60d63d87c7f627c737721
Rudolfs Dudarevs <rudolf.dudarevs@gmail.com>
2023-07-05 20:59:26 +0300

    piemers

--- 10-nodarbiba/uzdevumi/SandisAndrucis/sandis-andrucis-galadarbs/src/components/TextPicture/TextPicture.jsx
+++ 10-nodarbiba/uzdevumi/SandisAndrucis/sandis-andrucis-galadarbs/src/components/TextPicture/TextPicture.jsx
@@ -1,18 +1,27 @@
 import "./TextPicture.css";
 
-const TextPicture = ({title, text, description, image, textOrder, imageOrder}) => {
-    return (
-        <div className="text-picture__container">
-            <div className={textOrder + " text-picture__text-container"}>
-                <p className="text-picture__title">{title}</p>
-                <h2 className="text-picture__text">{text}</h2>
-                <p className="text-picture__description">{description}</p>
-            </div>
-            <div className={imageOrder + " text-picture__image-container"}>
-                <img src={image} className="text-picture__image"></img>
-            </div>
-        </div>
-    )
-}
+const TextPicture = ({ title, text, description, image, isReverse }) => {
+  return (
+    <div className="text-picture__container">
+      <div
+        className={`${
+          isReverse ? "text-picture__left-side" : "text-picture__right-side"
+        } text-picture__text-container`}
+      >
+        <p className="text-picture__title">{title}</p>
+        <h2 className="text-picture__text">{text}</h2>
+        <p className="text-picture__description">{description}</p>
+      </div>
+      <div
+        className={`${
+          isReverse ? "text-picture__right-side" : "text-picture__left-side"
+        } text-picture__image-container`}
+      >
+        {isReverse}
+        <img src={image} className="text-picture__image"></img>
+      </div>
+    </div>
+  );
+};
 
-export default TextPicture
+export default TextPicture;
