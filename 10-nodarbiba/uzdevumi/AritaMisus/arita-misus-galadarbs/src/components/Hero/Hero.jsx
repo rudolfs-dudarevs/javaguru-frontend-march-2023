74d07d3460bf296bd6750b43e066ec16efad61f8
CharisDev <99430685+CharisDev@users.noreply.github.com>
2023-07-10 12:20:20 +0300

    text size adjustments

--- 10-nodarbiba/uzdevumi/AritaMisus/arita-misus-galadarbs/src/components/Hero/Hero.jsx
+++ 10-nodarbiba/uzdevumi/AritaMisus/arita-misus-galadarbs/src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ import Button from "../Button/Button";
 const Hero = () => {
   return (
     <div className="hero">
-      <h3 className="hero-title">Food app</h3>
+      <h3 className="hero-pretitle">Food app</h3>
       <h1 className="hero-title">
         Why stay hungry when you can order from Bella Olonje
       </h1>
