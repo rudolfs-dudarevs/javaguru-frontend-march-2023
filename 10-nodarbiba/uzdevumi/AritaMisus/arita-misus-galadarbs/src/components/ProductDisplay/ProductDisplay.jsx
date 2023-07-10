2c4b74f78e38279f2397eb1ff354dcefe3c3b44c
CharisDev <99430685+CharisDev@users.noreply.github.com>
2023-07-10 12:55:03 +0300

    product display fix

--- 10-nodarbiba/uzdevumi/AritaMisus/arita-misus-galadarbs/src/components/ProductDisplay/ProductDisplay.jsx
+++ 10-nodarbiba/uzdevumi/AritaMisus/arita-misus-galadarbs/src/components/ProductDisplay/ProductDisplay.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import "./ProductDisplay.css";
-import PhoneLeft from "../../assets/phone-left.png";
-import PhoneRight from "../../assets/phone-right.png";
-import ProductImage from "../../assets/product-display.png";
+import ProductImage from "../../assets/phones.png";
 
 const ProductDisplay = () => {
   return (
     <div className="product-display">
-      {/* <img src={PhoneLeft} className="phone-left" />
-      <img src={PhoneRight} className="phone-right" /> */}
       <img src={ProductImage} className="product-image" />
     </div>
   );
