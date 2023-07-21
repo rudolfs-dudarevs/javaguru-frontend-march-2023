e006d78123e2a4cc20d7c96d6a1752dc877c5345
CharisDev <99430685+CharisDev@users.noreply.github.com>
2023-07-21 16:13:12 +0300

    header sticky on scroll

--- 10-nodarbiba/uzdevumi/AritaMisus/arita-misus-galadarbs/src/components/Header/Header.jsx
+++ 10-nodarbiba/uzdevumi/AritaMisus/arita-misus-galadarbs/src/components/Header/Header.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Header.scss";
 import Navigation from "../Navigation/Navigation";
 import Logo from "../../assets/logo.svg";
 
 const Header = () => {
+  const [scrollDirection, setScrollDirection] = useState(null);
+
+  useEffect(() => {
+    let lastScrollY = window.scrollY;
+
+    const updateScrollPosition = () => {
+      const scrollY = window.scrollY;
+      const direction = scrollY > lastScrollY ? "down" : "up";
+      if (
+        (direction !== scrollDirection && scrollY - lastScrollY > 10) ||
+        scrollY - lastScrollY < -10
+      ) {
+        setScrollDirection(direction);
+      }
+
+      lastScrollY = scrollY > 0 ? scrollY : 0;
+    };
+
+    console.log(lastScrollY);
+
+    window.addEventListener("scroll", updateScrollPosition);
+  }, [scrollDirection]);
+
   return (
-    <header className="header">
+    <header
+      className={`header ${scrollDirection === "down" ? "hide" : "show"}`}
+    >
       <Link to="/" className="logo">
         <img src={Logo} className="logo" />
       </Link>
